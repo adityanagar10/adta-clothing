ed3a74816453f70ea971dba19336f8190345032c
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-06-18 22:16:45 +0530

    Basic Nav Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-     <Homepage />
-    </div>
+  <Switch>
+    <Route exact path='/' component={Homepage}/>
+  </Switch> 
+   </div>
   );
 }
 
