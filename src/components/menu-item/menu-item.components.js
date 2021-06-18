c747a0a6a8ba3d2aab8959ba69280894ddd2b7eb
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-06-18 21:19:18 +0530

    items added

---
+++ src/components/menu-item/menu-item.components.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './menu-item.styles.scss'
+
+function MenuItem(props){
+    return(
+    <div className={`${props.size} menu-item`}>
+    <div className="background-image" style={{
+        backgroundImage: `url(${props.imageUrl})`
+    }}  />
+    <div className="content">
+        <h1 className="title">{props.title.toUpperCase()}</h1>
+        <span className="subtitle"> BUY NOW</span>
+    </div>
+    </div>
+    )
+}
+
+export default MenuItem;
