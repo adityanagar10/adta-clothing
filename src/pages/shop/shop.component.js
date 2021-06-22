30c7ac91c8787b100d531b3e9d997db05e7c2c6b
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-06-22 12:56:31 +0530

    Routing completed

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Routes} from 'react-router-dom'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.components';
 
 const ShopPage = () => (
 
 <div className="shop-page">
 {
-    <CollectionsOverview />
+    <Routes exact path= {`${match.path}`} component = {CollectionsOverview} />
 }
 </div>)
    
