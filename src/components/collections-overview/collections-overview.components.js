afed1a6b26b901374ff5fe4c4bed4abd3bec0006
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-06-24 12:15:53 +0530

    Data Flow improved

--- src/components/collections-overview/collections-overview.components.js
+++ src/components/collections-overview/collections-overview.components.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect} from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectShopSections} from '../../redux/shop/shop.selector'
+import { selectCollectionsForPreview} from '../../redux/shop/shop.selector'
 import CollectionPreview from '../preview/preview.collection'
 import './collections-overview.styles.scss'
 
@@ -17,7 +17,7 @@ const CollectionOverview = ({collections}) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopSections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionOverview)
